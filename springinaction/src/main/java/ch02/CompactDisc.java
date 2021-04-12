f6fab96677584ce7920db682e20627a7ccc8eb21
weiliangchun <840833211@qq.com>
2021-04-12 10:06:30 +0800

    docs(并发编程): spring实战

---
+++ springinaction/src/main/java/ch02/CompactDisc.java
@@ -0,0 +1,12 @@
+package ch02;
+
+/**
+ * @author weilc
+ * @version 1.0
+ * @description
+ * @className CompactDisc
+ * @date 2021.04.10
+ */
+public interface CompactDisc {
+    void play();
+}
---
+++ springinaction/src/main/java/ch03/MagicBean.java
@@ -0,0 +1,11 @@
+package ch03;
+
+/**
+ * @author weilc
+ * @version 1.0
+ * @description
+ * @className MagicBean
+ * @date 2021.04.10
+ */
+public class MagicBean {
+}
